77c0d7d3133581b2f964f1384bd04755e9bcc5d3
Olenka Vargas Lazarte <olenka@ravn.co>
2023-10-01 20:00:51 -0500

    fix layouts

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -1,3 +1,4 @@
 class OverviewController < ApplicationController
+  layout 'sidebar'
   def index; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  layout 'sidebar'
   before_action :find_by_id, only: %i[show edit update destroy]
 
   def index
